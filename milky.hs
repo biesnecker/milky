ee20d6360b81e1000366ef83450bda485e268f1d
John Biesnecker <jbiesnecker@gmail.com>
2012-12-13 10:05:36 +0800

    removed silly conversions

--- milky.hs
+++ milky.hs
@@ -1,12 +1,6 @@
 import System.Environment
 import Control.Monad
 
-kilogramsToPounds :: Float -> Float
-kilogramsToPounds x = x * 2.20462
-
-ouncesToMilliliters :: Float -> Float
-ouncesToMilliliters x = x * 29.5735
-
 handleArgs :: [String] -> [Float]
 handleArgs [x, y] = [read x :: Float, read y :: Float]
 handleArgs _ = error "Argument error"
@@ -14,6 +8,6 @@ handleArgs _ = error "Argument error"
 main :: IO ()
 main = do
   [w, t] <- liftM handleArgs getArgs
-  let lower_bound = ouncesToMilliliters (kilogramsToPounds w * 2) / t
+  let lower_bound = w * 130.39665914 / (24.0 / t)
   let upper_bound = lower_bound * 1.25
   putStrLn $ show (round lower_bound) ++ "\t" ++ show (round upper_bound)
